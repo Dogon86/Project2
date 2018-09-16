6fb49873b5eb9027ef8a331eca3b15d9bfd13a30
Dogon86 <dogon86@gmail.com>
2018-09-16 22:38:11 +0300

    modified:   css/main.css 	modified:   index.html 	modified:   js/js.js

---
+++ js/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	$("#back-top").hide();
+	
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+		
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
